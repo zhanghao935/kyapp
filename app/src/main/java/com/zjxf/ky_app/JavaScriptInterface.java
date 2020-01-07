975524814d04f0e7bd4aff1f117baf8e062c3044
田永来 <1297910164@qq.com>
2020-01-07 18:41:00 +0800

    update

--- app/src/main/java/com/zjxf/ky_app/JavaScriptInterface.java
+++ app/src/main/java/com/zjxf/ky_app/JavaScriptInterface.java
@@ -3,14 +3,13 @@ package com.zjxf.ky_app;
 
 import android.content.Context;
 import android.webkit.JavascriptInterface;
-
 import com.alibaba.fastjson.JSONObject;
 import com.zjxf.ky_app.utils.OkHttpUtils;
+import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-
-import okhttp3.Response;
 
 /**
  * created with IntelliJ IDEA
@@ -36,9 +35,19 @@ public class JavaScriptInterface {
     public String postData(String msg) throws IOException {
         JSONObject paramJson = JSONObject.parseObject(msg);
         String url = paramJson.getString("url");
-        Map<String, Object> paramMap = paramJson.getJSONObject("param").getInnerMap();
-        Response response = OkHttpUtils.getInstance().postData("http://192.168.0.139:8629" + url, paramMap);
-        return response.body().string();
+        JSONObject param = paramJson.getJSONObject("param");
+        if (param != null) {
+            Map<String, Object> paramMap = paramJson.getJSONObject("param").getInnerMap();
+            Response response = OkHttpUtils.getInstance().postData("http://192.168.0.139:8629" + url, paramMap);
+            System.out.println(response.body().string());
+            return response.body().string();
+        } else {
+            Response response = OkHttpUtils.getInstance().postData("http://192.168.0.139:8629" + url, new HashMap<String, Object>());
+            System.out.println(response.body().string());
+            return response.body().string();
+        }
+
+
     }
 
     /**
